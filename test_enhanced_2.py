4580ab199656bfea281f95334bac833bb5e2ce22
Morphiaaa <misslike_tjut@outlook.com>
2017-06-07 21:59:04 -0700

    first commit

---
+++ test_enhanced_2.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import os
+from skimage import io
+from skimage import color, exposure, transform
+import numpy as np
+import glob
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from keras.layers.normalization import BatchNormalization
+#import train_enhanced_1
+
+
+NUM_CLASSES = 43
+IMG_SIZE = 48
+
+test = pd.read_csv('GT-final_test.csv',sep=';')
+
+def preprocess_img(img):
+    # Histogram normalization in v channel
+    hsv = color.rgb2hsv(img)
+    hsv[:,:,2] = exposure.equalize_hist(hsv[:,:,2])
+    img = color.hsv2rgb(hsv)
+
+    # central square crop
+    min_side = min(img.shape[:-1])
+    centre = img.shape[0]//2, img.shape[1]//2
+    img = img[centre[0]-min_side//2:centre[0]+min_side//2,
+              centre[1]-min_side//2:centre[1]+min_side//2,
+              :]
+
+    # rescale to standard size
+    img = transform.resize(img, (IMG_SIZE, IMG_SIZE))
+
+    # roll color axis to axis 0
+    img = np.rollaxis(img,-1)
+
+    return img
+
+
+def cnn_model():
+    model = Sequential()
+
+    model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(3, IMG_SIZE, IMG_SIZE))) #activation='relu'))
+    model.add(BatchNormalization(axis = 1))
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(32, 3, 3))#, activation='relu'))
+    model.add(BatchNormalization(axis = 1))
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Convolution2D(64, 3, 3, border_mode='same'))#, activation='relu'))
+    model.add(BatchNormalization(axis = 1))
+    model.add(Activation('relu'))
+
+
+    model.add(Convolution2D(64, 3, 3))#, activation='relu'))
+    model.add(BatchNormalization(axis = 1))
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Convolution2D(128, 3, 3, border_mode='same'))#, activation='relu'))
+    model.add(BatchNormalization(axis = 1))
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(128, 3, 3))#, activation='relu'))
+    model.add(BatchNormalization(axis = 1))
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    model.add(Dense(512))#, activation='relu'))
+    model.add(BatchNormalization(axis = 1))
+    model.add(Activation('relu'))
+
+
+    model.add(Dropout(0.5))
+    model.add(Dense(NUM_CLASSES))#, activation='softmax'))
+    model.add(BatchNormalization(axis = 1))
+    model.add(Activation('softmax'))
+    return model
+
+model = cnn_model()
+lr = 0.01
+sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.load_weights('model2-2.h5')
+
+# Load test dataset
+X_test = []
+y_test = []
+i = 0
+for file_name, class_id  in zip(list(test['Filename']), list(test['ClassId'])):
+    img_path = os.path.join('GTSRB/Final_Test/Images/',file_name)
+    X_test.append(preprocess_img(io.imread(img_path)))
+    y_test.append(class_id)
+    
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+
+# predict and evaluate
+y_pred = model.predict_classes(X_test)
+acc = float(np.sum(y_pred==y_test))/float(np.size(y_pred))
+print("Test accuracy = {}".format(acc))
